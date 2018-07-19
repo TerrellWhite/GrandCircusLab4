4f092b558d79c238bb0d506d321ea81120b6d603
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-19 09:08:38 -0400

    First commit

---
+++ GrandCircus_Lab_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandCircus_Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string go;
+            
+            do
+            {
+                
+                Console.WriteLine("Learn your squares and cubes!");
+                Console.Write("Enter an integer:");
+                int number = int.Parse(Console.ReadLine());
+
+                const string format = "|{0,6} | {1,8} | {2,4}";
+                Console.WriteLine(string.Format(format, "Number", "Squared", "Cubed"));
+                Console.WriteLine(string.Format(format, "======", "=======", "======"));
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(string.Format(format, i, i*i, i*i*i));
+                    
+                }
+                Console.WriteLine("Continue? (y/n)");
+                go = Console.ReadLine();
+            } while (go.ToLower() == "yes" || go.ToLower() == "y");
+        }
+    }
+}
